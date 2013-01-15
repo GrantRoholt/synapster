5c118dfaf8ac71944f79cc9b925ffced5b70bb9b
GrantRoholt <grantroholt@gmail.com>
2013-01-14 19:42:22 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Synapster::Application.config.secret_token = 'da18f90a30da3ce83b9b4857b0c4d2f1448e29c12735572f01fe3fa8911a1d8a1ad299be7bf2cb2cf35c2efbc3d72b5d0d57819810f6af241c38c0f83da48804'
