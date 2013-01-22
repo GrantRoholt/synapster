78e3c24cd3d6d00126af0963a627447db82b5982
GrantRoholt <grantroholt@gmail.com>
2013-01-22 09:19:47 -0800

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+# returns the full title on a per-page basis
+def full_title(page_title)    # Definition & Assigment "full_title" method, with "page_title" argument
+	base_title = "Synapster"  # Variable assignment
+	if page_title.empty?      # Boolean test
+		base_title            # Implicit return
+	else
+		"#{base_title} | #{page_title}" # String interpolation
+	end
+  end
 end
