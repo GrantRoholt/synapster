9d0440bf297ccfbc18ad48f3a69673f8e4b1f992
GrantRoholt <grantroholt@gmail.com>
2013-02-11 16:13:54 -0800

    Finished sign in and sign out

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,10 +26,16 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   validates(:name, presence: true, length: { maximum: 50 })   # "validates" is just a method, as is "attr_accessible" above
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates(:email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false })   # "validates" is just a method, as is "attr_accessible" above
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
